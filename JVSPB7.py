93f0b3f06525c94f6d30db5807543d386c11cb14
jomarmdb <matt.bianchi@jomar.com>
2025-10-17 10:32:23 -0700

    Update JVSPB7.py

--- JVSPB7.py
+++ JVSPB7.py
@@ -325,7 +325,7 @@ html, body, [class*="css"], .stMarkdown, .stButton button, .stTextInput input, .
 }
 </style>
 """, unsafe_allow_html=True)
-st.header("JOMAR VALVE SUBMITTAL PACKAGE CREATOR")
+st.title("JOMAR VALVE SUBMITTAL PACKAGE CREATOR")
 st.markdown("##### Upload PDFs and/or select from below catalog, reorder in the sidebar, and generate a combined PDF with a custom cover.")
 
 # Resolve app dir + default logo path (next to this file)
